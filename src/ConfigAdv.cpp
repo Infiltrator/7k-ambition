6fd6925eccb49cbd304b3bed440158180603fa04
Tim Sviridov <Lt.Infiltrator@gmail.com>
2024-12-18 21:04:45 +1000

    Allow comments (starting with #) in config.txt

--- src/ConfigAdv.cpp
+++ src/ConfigAdv.cpp
@@ -203,6 +203,11 @@ int ConfigAdv::load(char *filename)
 		line++;
 
 		fileTxt.match_chars(" \t");
+		if(fileTxt.match_chars("#"))
+		{
+			fileTxt.next_line();
+			continue;
+		}
 		name = fileTxt.data_ptr;
 		if( !fileTxt.match_chars_ex("= \t\r\n\x1a") )
 		{
