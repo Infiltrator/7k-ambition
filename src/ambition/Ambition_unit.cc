c32bd6dc608a3b74cc0609b5bb23da58272fe3fa
Tim Sviridov <Lt.Infiltrator@gmail.com>
2025-06-26 00:56:34 +1000

    Clean up typo of INSIDE_BUILDING_DISTANCE_PENALTY

--- src/ambition/Ambition_unit.cc
+++ src/ambition/Ambition_unit.cc
@@ -92,8 +92,8 @@ bool sendAvailableBuilderToFirm(
     );
 
     if (unit->unit_mode == UNIT_MODE_CONSTRUCT) {
-      constexpr auto INSIDE_BUILDING_DISTANCE_PERANTLY = 16;
-      distance += 16;
+      constexpr auto INSIDE_BUILDING_DISTANCE_PENALTY = 16;
+      distance += INSIDE_BUILDING_DISTANCE_PENALTY;
 
       const auto firm = firm_array[unit->unit_mode_para];
       if (firm->hit_points < firm->max_hit_points) {
