be53e543aed2745585cb102621a79c3f6f7b8720
Tim Sviridov <Lt.Infiltrator@gmail.com>
2025-07-01 00:56:20 +1000

    Prevent accidentally hiring the wrong Inn guest

--- src/ambition/7kaaInterface/inn.cc
+++ src/ambition/7kaaInterface/inn.cc
@@ -26,11 +26,47 @@
 #define _AMBITION_IMPLEMENTATION
 #include "inn.hh"
 
+#include <cstdint>
+
+#include "OBUTT3D.h"
+#include "OF_INN.h"
+#include "OMOUSE.h"
+#include "OVBROWIF.h"
+
 #include "Ambition_config.hh"
 #include "Ambition_inn.hh"
 
 
 namespace _7kaaAmbitionInterface::Inn {
+
+bool guestSelectionValid = false;
+
+
+void detectBrowserClick(
+  FirmInn* inn,
+  const VBrowseIF& guestBrowser
+) {
+  if (!Ambition::config.enhancementsAvailable()) {
+    return;
+  }
+
+  if (mouse.any_click(
+      guestBrowser.ix1,
+      guestBrowser.iy1,
+      guestBrowser.ix2,
+      guestBrowser.iy2
+    )
+  ) {
+    const auto recordNumber = getSelectedRecordNumber(
+      inn,
+      (guestBrowser.top_rec_no
+        + ((mouse.click_y() - guestBrowser.iy1)
+           / (guestBrowser.rec_height + guestBrowser.rec_y_space)
+           * guestBrowser.x_max_rec))
+    );
+    guestSelected(inn, recordNumber);
+  }
+}
 
 int getSelectedRecordNumber(
   const FirmInn* inn,
@@ -41,6 +77,51 @@ int getSelectedRecordNumber(
   }
 
   return Ambition::getInnSelectedRecordNumber(inn, browserRecordNumber);
+}
+
+void guestLeft(
+  FirmInn* inn,
+  const VBrowseIF& guestBrowser,
+  const int recordNumber
+) {
+  if (!Ambition::config.enhancementsAvailable()) {
+    return;
+  }
+
+  if (inn->firm_recno != firm_array.selected_recno || !inn->should_show_info()) {
+    return;
+  }
+
+  if (guestBrowser.rec_no == recordNumber) {
+    guestSelectionValid = false;
+  }
+}
+
+void guestSelected(
+  FirmInn* inn,
+  const int recordNumber
+) {
+  if (!Ambition::config.enhancementsAvailable()) {
+    return;
+  }
+
+  if (inn->firm_recno != firm_array.selected_recno || !inn->should_show_info()) {
+    return;
+  }
+
+  guestSelectionValid = true;
+}
+
+void overrideHireButtonStatus(
+  Button3D& hireButton
+) {
+  if (!Ambition::config.enhancementsAvailable()) {
+    return;
+  }
+
+  if (!guestSelectionValid) {
+    hireButton.disable();
+  }
 }
 
 void refreshBrowser(
